0c78e10bdeb3f412594f0379e18848b4e73459fd
Mathias Gajhede <mag@venturecapitals.dk>
2023-10-21 23:47:30 +0200

    Update import.php

--- import.php
+++ import.php
@@ -3,7 +3,7 @@ $new_file = $cart_file_handling_name."/$cart_partner_name.php";
 rename($temp_folder_name."ps_ippgateway", $cart_file_handling_name);
 
 
-rename("$cart_file_handling_name/paymentexampl.php", $new_file);
+rename("$cart_file_handling_name/paymentexample.php", $new_file);
 $str=file_get_contents($new_file);
 $str=str_replace("PaymentExample", "PAYMENT".$partner_name,$str);
 $str=str_replace("PAYMENTEXAMPLE", "PAYMENT".strtoupper($partner_name),$str);
